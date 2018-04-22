fb76385d5aebbd17486e8e9c9893e975853ddabb
周帅 <571840649@qq.com>
2018-04-22 19:05:00 +0800

    update pointer

--- middlewares/error.go
+++ middlewares/error.go
@@ -6,7 +6,7 @@ import (
 )
 
 // ErrorHandler 通用错误处理
-func ErrorHandler(ctx gin.Context) {
+func ErrorHandler(ctx *gin.Context) {
 	defer func() {
 		if err := recover(); err != nil {
 			ctx.JSON(http.StatusInternalServerError, err)
