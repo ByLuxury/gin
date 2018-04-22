f00b1368497e7202d3f1e758ddf75fa0d3c27718
周帅 <571840649@qq.com>
2018-04-22 17:24:05 +0800

    common error handle

---
+++ error.go
@@ -0,0 +1,17 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// ErrorHandler 通用错误处理
+func ErrorHandler(ctx gin.Context) {
+	defer func() {
+		if err := recover(); err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+		}
+	}()
+
+	ctx.Next()
+}
