e1aa563354909e9e81882e5c6b2e2ce955b0f14c
zhoushuaime <zhoushuaime@foxmail.com>
2021-05-25 17:34:05 +0800

    print stack

--- middlewares/error.go
+++ middlewares/error.go
@@ -2,18 +2,23 @@ package middlewares
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 // ErrorHandler 通用错误处理
 func ErrorHandler(ctx *gin.Context) {
 	defer func() {
 		if err := recover(); err != nil {
+			//打印错误堆栈信息
+			log.Printf("panic: %v\n", err)
+			debug.PrintStack()
 			if ctx.Writer.Status() != http.StatusOK {
 				ctx.Status(http.StatusOK)
 			}
-				ctx.JSON(http.StatusOK, err)
- 		}
+			ctx.JSON(http.StatusOK, err)
+		}
 	}()
 	ctx.Next()
 }
